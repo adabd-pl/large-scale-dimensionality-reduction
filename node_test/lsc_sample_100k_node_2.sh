5d6646afe9fd48f1dbe5e98c000cbbd225fd3741
adabd-pl <72196988+adabd-pl@users.noreply.github.com>
2025-01-16 17:01:58 +0100

    Add node test

---
+++ node_test/lsc_sample_100k_node_2.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#SBATCH --job-name=lsc_sample_100k_node_2_cpus_1
+#SBATCH --time=05:00:00
+#SBATCH --partition=plgrid
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=180G
+#SBATCH --nodes 2
+#SBATCH --ntasks-per-node=1 
+#SBATCH --account=plglscclass24-cpu
+#SBATCH --output=lsc_sample_100k_node_2.txt
+
+
+PYTHON_EXEC="/net/ascratch/people/plgadabd/.conda/envs/ab_env_test/bin/python"
+
+SAMPLE_SIZES=(100000)
+
+
+for SAMPLE_SIZE in "${SAMPLE_SIZES[@]}"; do
+    echo "====================="
+    echo "PACMAP - $SAMPLE_SIZE"
+    $PYTHON_EXEC pacmap_test/lsc_pacmap.py $SAMPLE_SIZE
+
+    echo "====================="
+    echo "PCA - $SAMPLE_SIZE"
+    $PYTHON_EXEC pca_test/lsc_pca.py $SAMPLE_SIZE
+
+    echo "====================="
+    echo "UMAP - $SAMPLE_SIZE"
+    $PYTHON_EXEC umap_test/lsc_umap.py $SAMPLE_SIZE
+
+    echo "====================="
+    echo "TSNE - $SAMPLE_SIZE"
+    $PYTHON_EXEC tsne_test/lsc_tsne.py $SAMPLE_SIZE
+done
