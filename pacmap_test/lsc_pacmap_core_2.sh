487432ccdbe3e4943fe5ec49aef8f83c087bb8ac
adabd-pl <72196988+adabd-pl@users.noreply.github.com>
2025-01-16 16:55:01 +0100

    Add test scripts

---
+++ pacmap_test/lsc_pacmap_core_2.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --job-name=lsc_sample_pacmap_mix_core_2
+#SBATCH --time=02:00:00
+#SBATCH --partition=plgrid
+#SBATCH --cpus-per-task=2
+#SBATCH --mem-per-cpu=32G
+#SBATCH --nodes 1
+#SBATCH --ntasks-per-node=1 
+#SBATCH --account=plglscclass24-cpu
+#SBATCH --output=lsc_sample_pacmap_mix_core_2.txt
+
+
+PYTHON_EXEC="/net/ascratch/people/plgadabd/.conda/envs/ab_env_test/bin/python"
+
+SAMPLE_SIZES=(100 1000 10000 100000 500000 1000000 2000000)
+
+for SAMPLE_SIZE in "${SAMPLE_SIZES[@]}"; do
+    echo "====================="
+    echo "PACMAP - $SAMPLE_SIZE"
+    $PYTHON_EXEC lsc_pacmap.py $SAMPLE_SIZE
+done
---
+++ pca_test/lsc_pca_core_4.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --job-name=lsc_sample_pca_mix_core_4
+#SBATCH --time=02:00:00
+#SBATCH --partition=plgrid
+#SBATCH --cpus-per-task=4
+#SBATCH --mem-per-cpu=16G
+#SBATCH --nodes 1
+#SBATCH --ntasks-per-node=1 
+#SBATCH --account=plglscclass24-cpu
+#SBATCH --output=lsc_sample_pca_mix_core_4.txt
+
+
+PYTHON_EXEC="/net/ascratch/people/plgadabd/.conda/envs/ab_env_test/bin/python"
+
+SAMPLE_SIZES=(100 1000 10000 100000 500000 1000000 2000000)
+
+for SAMPLE_SIZE in "${SAMPLE_SIZES[@]}"; do
+    echo "====================="
+    echo "PCA - $SAMPLE_SIZE"
+    $PYTHON_EXEC lsc_pca.py $SAMPLE_SIZE
+done
